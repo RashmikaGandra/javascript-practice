e9eeec7f32cabf85b67b431b7f7280d563c3a214
Rashuu28 <rashmikagandra@gmail.com>
2025-09-19 14:36:55 +0530

    All Arthmetic Operations

---
+++ arthmetic_operations.js
@@ -0,0 +1,19 @@
+let number1 = 10;
+let number2 = 20;
+console.log("Arthmetic Operators");
+const sum = number1 + number2;
+console.log("sum of ",number1,"and",number2,"is",sum);
+const subtraction = number1 - number2
+console.log("subtraction of ",number1,"and",number2,"is",subtraction);
+const product = number1 * number2
+console.log("product of ",number1,"and",number2,"is",product);
+const division = number1 / number2
+console.log("division of ",number1,"and",number2,"is",division);
+const modulus = number1 % number2
+console.log("modulus of ",number1,"and",number2,"is",modulus);
+const increment = number1++
+console.log("increment of ",number1,"is",increment);
+const decrement = --number2;
+console.log("decrement of ",number2,"is",decrement);
+const syntaticSugar = number1 += 10;
+console.log(syntaticSugar);
