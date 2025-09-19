1a0a68255804cd483ddc750f5efc99b4d0af89e0
RashmikaGandra <rashmikagandra@gmail.com>
2025-09-19 17:10:33 +0530

    checking mut

---
+++ switch.js
@@ -0,0 +1,15 @@
+let numberOfDresses = 5;
+switch (numberOfDresses) {
+    case 5 :
+        console.log("somewhat fashionist");
+        break;
+    case 10 :
+        console.log("not bad");
+        break;
+    case 15 :
+        console.log("great fashionist");
+        break;
+    default:
+        console.log("keep it up");
+        break;
+}
