ac5b25d7c7be02578161fe80c821d6c6454a1daf
RashmikaGandra <rashmikagandra@gmail.com>
2025-09-19 17:02:21 +0530

    Checking two numbers are equal or not

---
+++ if_else/equal_or_not.js
@@ -0,0 +1,9 @@
+const num1 = 15;
+const num2 = 78;
+if (num1 === num2) {
+    console.log(num1," is equal to ",num2);
+} else {
+    console.log(num1," is not equal to ",num2);
+}
+console.log('control flow executed');
+
