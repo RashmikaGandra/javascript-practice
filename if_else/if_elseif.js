350f0f3b776e3e0c2daa26fe852684f1c4e17534
RashmikaGandra <rashmikagandra@gmail.com>
2025-09-19 17:05:15 +0530

    checking multiple conditions

---
+++ if_else/if_elseif.js
@@ -0,0 +1,8 @@
+const numberOfDresses = 20;
+if (numberOfDresses < 5) {
+    console.log("somewhat fashionist");
+} else if (numberOfDresses === 10) {
+    console.log(" not bad ");
+} else if (numberOfDresses > 10) {
+    console.log("great fanshionist");
+}
