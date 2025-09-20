337e44d3684ceca2ed27d4c4e9be526dbb988cc1
RashmikaGandra <rashmikagandra@gmail.com>
2025-09-20 20:53:09 +0530

    Accepting all test cases

--- lion_hunt.js
+++ lion_hunt.js
@@ -1,18 +1,34 @@
-const input = "L     Z";
+const input = " L Z";
 let space = 0;
 let newSpace;
-for (let index = 0 ; index < input.length ; index= index + 1) {
-    if(input[index] === "Z") {
-        if(input[index + 1] === "L") {
+for (let index = 0 ; index < input.length ; index++) {
+    if(input[index] === "Z" || input[index] === "L") {
+        if(input[index + 1] === "L" || input[index + 1] === "Z") {
             newSpace = 0;
         }
     }
-    else if(input[index] === " ") {
+    else if(input[index] === " " && index !== 0) {
         space = space + 1;
-        
     }
 }
-if (newSpace < space) {
+let newString = "";
+for (let index = 0 ; index < input.length ; index++) {
+    if(input[index] !== " "){
+        newString = newString + input[index];
+    }
+    
+}
+for(let index = 0;index < newString.length;index++){
+    if(newString[index]=== newString[index + 1] || newString[index] === " "){
+        space = -1;
+    }
+    
+}
+if(newString.length === 1) {
+    space = -1;
+}
+if (newSpace <= space) {
     space = newSpace;
 }
 console.log(space);
+ 
