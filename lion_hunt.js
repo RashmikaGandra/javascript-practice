3900be1759396fb10e2db906a6d7621919e9a7eb
RashmikaGandra <rashmikagandra@gmail.com>
2025-09-22 09:20:53 +0530

    Accepting some more test cases

--- lion_hunt.js
+++ lion_hunt.js
@@ -1,34 +1,44 @@
-const input = " L Z";
+const input = "L  L";
 let space = 0;
-let newSpace;
+let noSpace;
+let shortestSpace;
 for (let index = 0 ; index < input.length ; index++) {
-    if(input[index] === "Z" || input[index] === "L") {
+    if(input[index] === "Z" || input[index] === "L" ) {
         if(input[index + 1] === "L" || input[index + 1] === "Z") {
-            newSpace = 0;
+            noSpace = 0;
         }
     }
-    else if(input[index] === " " && index !== 0) {
+    if ((input[index] === "Z" || input[index] === "L" )&& input[index + 1] === " "){
+        shortestSpace = space;
+        space = 0;
+    }
+    if(input[index] === " " && index !== 0) {
         space = space + 1;
     }
 }
 let newString = "";
-for (let index = 0 ; index < input.length ; index++) {
-    if(input[index] !== " "){
+for (let index = 0 ; index < input.length ; index= index + 1) {
+     if(input[index] !== " "){
         newString = newString + input[index];
     }
-    
 }
-for(let index = 0;index < newString.length;index++){
-    if(newString[index]=== newString[index + 1] || newString[index] === " "){
-        space = -1;
+for(let newIndex = 0;newIndex < newString.length;newIndex++){
+    if(newString[newIndex] === newString[newIndex + 1] || newString[newIndex] === " "){
+        shortestSpace = -1;
+        newIndex = newString.length;
+    } 
+    if(newString[newIndex] !== newString[newIndex + 1]) {
+        shortestSpace = space;
+        newIndex = newString.length;
     }
-    
 }
 if(newString.length === 1) {
-    space = -1;
+    shortestSpace = -1;
 }
-if (newSpace <= space) {
-    space = newSpace;
+if (noSpace <= shortestSpace) {
+    shortestSpace = noSpace;
 }
-console.log(space);
- 
+if(space <= shortestSpace) {
+    shortestSpace = space;
+} 
+console.log(shortestSpace);
