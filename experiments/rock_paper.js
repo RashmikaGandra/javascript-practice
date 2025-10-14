65d22e2717e0024e96821760b9fae2f5bd6c0165
RashmikaGandra <rashmikagandra@gmail.com>
2025-10-14 11:25:10 +0530

    Update rock_paper.js

--- experiments/rock_paper.js
+++ experiments/rock_paper.js
@@ -1,56 +1,68 @@
-function generatePoints(winner) {
-  let userPoints = 0;
-  let systemPoints = 0;
-  switch(winner) {
-    case "user" : userPoints++;
-    case "system" : systemPoints++;
-  }
-  const points = `${"user-Points"} : ${userPoints}  ${"system-points"} : ${systemPoints}`;
-  console.log(points);
-  return winner;
+const SYMBOLS = ["🪨", "📄", "✂️"];
+
+function displayPoints(points, winner) {
+  const systemPoints = points[1];
+  const userPoints = points[0];
+  const score = `${"user-Points"} : ${userPoints}  ${"system-points"} : ${systemPoints}`;
+  console.log(score);
+  console.log(winner);
+  return takeInput(SYMBOLS, points);
 }
 
-function checkTheMoves(userMove, systemMove) {
+function generatePoints(winner, points) {
+  while (points[0] !== 2 && points[1] !== 2) {
+    switch(winner) {
+      case "user" : points[0]++;
+        break;
+      case "system" : points[1]++;
+        break;
+    }
+    return displayPoints(points, winner);
+  }
+  console.log(points[0] > points[1] ? "user Won" : "system Won");
+}
+
+function checkTheMoves(userMove, systemMove, points) {
   if (userMove === systemMove) {
-    return generatePoints("TIE");
+    return generatePoints("TIE", points);
   }
   if (userMove === 0 && systemMove === 1) {
-    return generatePoints("system");
+    return generatePoints("system", points);
   }
   if (userMove === 0 && systemMove === 2) {
-    return generatePoints("user");
+    return generatePoints("user", points);
   }
   if (userMove === 1 && systemMove === 0) {
-    return generatePoints("user");
+    return generatePoints("user", points);
   }
   if (userMove === 1 && systemMove === 2) {
-    return generatePoints("system");
+    return generatePoints("system", points);
   }
   if (userMove === 2 && systemMove === 0) {
-    return generatePoints("system");
+    return generatePoints("system", points);
   }
   if (userMove === 2 && systemMove === 1) {
-    return generatePoints("user");
+    return generatePoints("user", points);
   }
 }
 
-function play(userMove, systemMove, symbols) {
-  const winner = checkTheMoves(userMove, systemMove);
-  console.log("usermove :", symbols[userMove], "systemMove :", symbols[systemMove]);
-  return winner;
+function play(userMove, systemMove, SYMBOLS, points) {
+  const winner = checkTheMoves(userMove, systemMove, points);
+  console.log("usermove :", SYMBOLS[userMove], "systemMove :", SYMBOLS[systemMove]);
+  return winner + "🏆🏆🏆🏆🏆";
 }
 
-function takeInput(symbols) {
+function takeInput(SYMBOLS, points) {
   const uesrMove = parseInt(prompt("Enter user move:"));
-  const systemMove = Math.floor(Math.random() * symbols.length);
-  return play(uesrMove, systemMove, symbols);
+  const systemMove = Math.floor(Math.random() * SYMBOLS.length);
+  return play(uesrMove, systemMove, SYMBOLS, points);
 }
 
 function displayCharacters() {
-  const symbols = ["🪨", "📄", "✂️"];
-  const message = `${0}:${symbols[0]} ${1}:${symbols[1]} ${2}:${symbols[2]} `;
+  const points = [0, 0];
+  const message = `${0}:${SYMBOLS[0]} ${1}:${SYMBOLS[1]} ${2}:${SYMBOLS[2]} `;
   console.log(message);
-  return takeInput(symbols);
+  return takeInput(SYMBOLS, points);
 }
 
 function playAgain() {
@@ -59,9 +71,9 @@ function playAgain() {
 }
 
 function main() {
-  console.log(displayCharacters());
+  displayCharacters();
   while (playAgain()) {
-    console.log(displayCharacters());
+    displayCharacters();
   }
 }
 
