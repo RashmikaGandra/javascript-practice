ccd55883c9a8a0d5f2404db690fadba3e90938d3
RashmikaGandra <rashmikagandra@gmail.com>
2025-11-05 17:59:30 +0530

    Create simple_function.js

---
+++ functions_practice/simple_function.js
@@ -0,0 +1,13 @@
+function expressFeeling(feeling) {
+    return "I am "+feeling
+}
+console.log(expressFeeling("sad"));
+
+function calling(x) {
+    return console.log();
+}
+console.log(calling(2));
+
+function typeOfValue(value) {
+    return "type of " + value + typeof(value);
+}
